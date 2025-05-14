48cf45e32bc0488b5814f1db04530a0c3a118291
Hasan <hassandossa5@gmail.com>
2025-05-14 12:15:37 +0300

    Day 2 - route groups

---
+++ hello-world/src/app/(auth)/forgotPassword/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div>
+            <h1>Contact our admin to reset your password</h1>
+        </div>
+    )
+}
+
+export default page
---
+++ hello-world/src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <h1>Welcom back, login to continue</h1>
+        </div>
+    )
+}
+
+export default Login
