2e6b4e260880d1cdfad675466dcbc76cc24b8800
Hasan <hassandossa5@gmail.com>
2025-05-14 12:05:32 +0300

    Day 2 - routes with underscore implementation

---
+++ hello-world/src/app/%5Flib/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RouteWithUnderscore() {
+  return (
+    <div>
+      <h1>Route with _ are usually private, but if you intentionally want an underscore in the route, prefix the file name with %5F</h1>
+    </div>
+  )
+}
+
+export default RouteWithUnderscore
