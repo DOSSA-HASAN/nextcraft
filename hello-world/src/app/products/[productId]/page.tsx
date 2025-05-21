4d0ae1f3ed8f562172808bd977a2da7e31a047de
Hasan <hassandossa5@gmail.com>
2025-05-21 09:34:54 +0300

    Day 4 - Understanding metadata title

--- hello-world/src/app/products/[productId]/page.tsx
+++ hello-world/src/app/products/[productId]/page.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { Metadata } from 'next'
+import { resolve } from 'path'
 
-async function ProductDetail({ params }: { params: Promise<{ productId: string }> }) {
+type Props = { params: Promise<{ productId: String }> }
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const id = (await params).productId
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Iphone ${id}`)
+        }, 3000);
+    })
+    return {
+        title: `Product ${title}`
+    }
+}
+
+async function ProductDetail({ params }: Props) {
     const productId = (await params).productId
     return (
         <div>
