7ea9b126fc37e00173d19985966ac6e166a1189f
Hasan <hassandossa5@gmail.com>
2025-05-21 10:07:09 +0300

    Day 4 - Understanding Link component and its href and replace attributes

--- hello-world/src/app/blog/page.tsx
+++ hello-world/src/app/blog/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import React from 'react'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
     title: { absolute: "Blog" }
@@ -9,6 +10,8 @@ function Blog() {
     return (
         <section>
             <h1>My blog</h1>
+            <Link href={'/'}>Back home</Link>
+            <Link href={'/products'}>Products</Link>
         </section>
     )
 }
--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 function page() {
     return (
         <section>
             <h1>Welcome Home</h1>
+            <Link href={'/blog'}>Blog</Link>
+            <Link href={'/products'}>Product List</Link>
         </section>
     )
 }
