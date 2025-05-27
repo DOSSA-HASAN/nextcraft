69cc62a611c73ed5cda13ac2f43a42373f20a90c
Hasan <hassandossa5@gmail.com>
2025-05-27 11:50:54 +0300

    Day 7 - Understanding intercepting routes

---
+++ hello-world/src/app/f1/(.)f2/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InterceptedF2() {
+  return (
+    <div>
+      <h1>Intercepted f2 page</h1>
+    </div>
+  )
+}
+
+export default InterceptedF2
---
+++ hello-world/src/app/f1/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+function F1() {
+    return (
+        <div>
+            <h1>F1 Page</h1>
+            <div className='bg-blue-500 rounded-md p-3 m-3 text-white hover:cursor-pointer font-bold'>
+                <Link href={'/f1/f2'}>Go to F2</Link>
+            </div>
+        </div>
+    )
+}
+
+export default F1
