c8b9ccf79807fb121c4c859d50bab6687dc37d71
Hasan <hassandossa5@gmail.com>
2025-06-04 10:44:13 +0300

    Day 8 - working with cookies

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from "next/server"
-import { headers } from "next/headers"
+import { headers, cookies } from "next/headers"
 
 export async function GET(req: NextRequest) {
     // const reqHeaders = new Headers(req.headers)
@@ -7,8 +7,18 @@ export async function GET(req: NextRequest) {
 
     const headerList = await headers();
     console.log(headerList.get("Authorization"))
+    
+    // const theme = req.cookies.get("theme")
+    // console.log(theme)
+
+    const cookieStore = await cookies();
+    cookieStore.set("resultsPerPage", "20")
+    console.log(cookieStore.get("resultsPerPage"))
 
     return new Response("<h1>Profile page data</h1>", {
-        headers: { "Content-Type": "text/html" }
+        headers: { 
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
+         },
     })
 }
