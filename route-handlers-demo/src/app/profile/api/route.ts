0e029ebdc08a6cd6e1abf5d0b116717f23d3d07f
Hasan <hassandossa5@gmail.com>
2025-06-04 10:40:04 +0300

    Day 8 - Understanding headers with route handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,14 @@
-export async function GET() {
-    return new Response("Profile page data")
+import { type NextRequest } from "next/server"
+import { headers } from "next/headers"
+
+export async function GET(req: NextRequest) {
+    // const reqHeaders = new Headers(req.headers)
+    // console.log(reqHeaders.get("Authorization"))
+
+    const headerList = await headers();
+    console.log(headerList.get("Authorization"))
+
+    return new Response("<h1>Profile page data</h1>", {
+        headers: { "Content-Type": "text/html" }
+    })
 }
