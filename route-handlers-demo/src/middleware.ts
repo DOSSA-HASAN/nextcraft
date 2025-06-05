c85c642f625c984bc908a8a1699238e814ee4e35
Hasan <hassandossa5@gmail.com>
2025-06-05 10:53:52 +0300

    Day 9 - Understanding conditional response redirects

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -1,9 +1,12 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-    return NextResponse.redirect(new URL("/", req.url))
+    // return NextResponse.redirect(new URL("/", req.url))
+    if(req.nextUrl.pathname === "/profile"){
+        return NextResponse.redirect(new URL("/hello", req.url))
+    }
 }
 
-export const config = {
-    matcher: "/profile"
-}
+// export const config = {
+//     matcher: "/profile"
+// }
