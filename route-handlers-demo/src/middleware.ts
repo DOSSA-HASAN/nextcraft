0ccd36f3384477f1079099daf9683dbe904ff8bb
Hasan <hassandossa5@gmail.com>
2025-06-05 11:02:59 +0300

    Day 9 - Understanding cookies with response object

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -1,10 +1,19 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-    // return NextResponse.redirect(new URL("/", req.url))
-    if(req.nextUrl.pathname === "/profile"){
-        return NextResponse.rewrite(new URL("/hello", req.url))
+
+    const response = NextResponse.next();
+    const theme = response.cookies.get("theme")
+    if(!theme){
+        response.cookies.set("theme", "idkbro")
     }
+
+    return response;
+
+    // return NextResponse.redirect(new URL("/", req.url))
+    // if(req.nextUrl.pathname === "/profile"){
+    //     return NextResponse.rewrite(new URL("/hello", req.url))
+    // }
 }
 
 // export const config = {
