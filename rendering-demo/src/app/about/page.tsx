7a7f337398f3a1962680f4fa5b1ed938a1ad0f62
Hasan <hassandossa5@gmail.com>
2025-06-10 08:21:16 +0300

    Day 10 - Understanding Server & client components

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -4,7 +4,8 @@ function About() {
     console.log("About server component")
     return (
         <section>
-
+            <h1>About page</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
         </section>
     )
 }
