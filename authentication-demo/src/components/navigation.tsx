fbd8a93e44963fa83e9e37d825cac0dfdbf40f9a
Hasan <hassandossa5@gmail.com>
2025-07-31 09:29:15 +0300

    Day 15 - Clerk Sign In & Sign Out

---
+++ authentication-demo/src/components/navigation.tsx
@@ -0,0 +1,17 @@
+import { SignInButton, SignOutButton } from '@clerk/nextjs'
+import React from 'react'
+
+function Navigation() {
+    return (
+        <div className='flex justify-between items-center p-3 border-b-1 border-white'>
+            <h1 className='text-[30px] text-white font-bold'>Auth<span className='text-blue-300'>Clerk</span></h1>
+            {/* <button className='rounded-md p-2 bg-white text-black font-bold hover:cursor-pointer'>Sign In</button> */}
+            <div className='flex jusitfy-between items-center'>
+                <SignInButton mode='modal'/>
+                <SignOutButton />
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
