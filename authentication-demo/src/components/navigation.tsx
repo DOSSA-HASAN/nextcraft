7527a675f63a1f1039b471e3deab0f84e26c581d
Hasan <hassandossa5@gmail.com>
2025-08-01 09:18:21 +0300

    Day 16 - Conditional UI rendering with Clerk auth status

--- authentication-demo/src/components/navigation.tsx
+++ authentication-demo/src/components/navigation.tsx
@@ -1,4 +1,5 @@
-import { SignInButton, SignOutButton } from '@clerk/nextjs'
+'use client'
+import { SignInButton, SignOutButton, SignedIn, SignedOut } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,11 +8,15 @@ function Navigation() {
         <div className='flex justify-between items-center p-3 border-b-1 border-white'>
             <h1 className='text-[30px] text-white font-bold'>Auth<span className='text-blue-300'>Clerk</span></h1>
             {/* <button className='rounded-md p-2 bg-white text-black font-bold hover:cursor-pointer'>Sign In</button> */}
-            <div className='flex jusitfy-between items-center'>
-                <SignInButton mode='modal' />
-                <SignOutButton />
+            <div className='flex justify-between items-center'>
+                <SignedOut>
+                    <SignInButton mode='modal' />
+                </SignedOut>
                 {/* <UserButton /> */}
-                <Link href={"/user-profile"}>Profile</Link>
+                <SignedIn>
+                    <SignOutButton />
+                    <Link href={"/user-profile"}>Profile</Link>
+                </SignedIn>
             </div>
         </div>
     )
